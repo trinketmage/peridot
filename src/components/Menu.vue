34ba3ae5bc8f1f16253e945ef1fec057edc60022
trinktmage <tranremi@live.fr>
2019-08-12 00:17:22 +0200

    Menu > interaction

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="menu" id="menu">
-    <h3>Missions</h3>
-    <div
-      class="repeater"
-      :key="idx"
-      v-for="(scenario, idx) in scenarios"
-    >
-      <router-link
-        class="underline"
-        :to="{ name: 'mission', params: { locale: $route.params.locale, id: idx + 1 } }"
+    <div class="sizer">
+      <h3>Missions</h3>
+      <div
+        class="repeater"
+        :key="idx"
+        v-for="(scenario, idx) in scenarios"
       >
-        <span class="bold">{{ idx + 1 }}</span>. {{ scenario.name }}
-      </router-link>
-    </div>
+        <router-link
+          class="underline"
+          :to="{ name: 'mission', params: { locale: $route.params.locale, id: idx + 1 } }"
+        >
+          <span class="bold">{{ idx + 1 }}</span>. {{ scenario.name }}
+        </router-link>
+      </div>
+  </div>
+    <div class="tongue" />
   </div>
 </template>
 
@@ -33,8 +36,6 @@ export default {
   display: flex;
   flex-direction: column;
   position: fixed;
-  background-color: rgba(255, 255, 255, .875);
-  box-shadow: 0 13px 27px -5px rgba(50,50,93,.25), 0 8px 16px -8px rgba(0,0,0,.3), 0 -6px 16px -6px rgba(0,0,0,.025);
   height: 100%;
   left: 0;
   top: 0;
@@ -42,11 +43,21 @@ export default {
   z-index: 1;
   min-width: 252px;
   max-width: 400px;
-  overflow: auto;
-  padding: 48px;
   transition: transform 1s ease;
   transform: translateX(-100%);
   will-change: transform;
+  &:hover {
+    transform: translateX(0%);
+  }
+}
+.sizer {
+  width: 100%;
+  height: 100%;
+  padding: 48px;
+  background-color: rgba(255, 255, 255, .875);
+  box-shadow: 0 13px 27px -5px rgba(50,50,93,.25), 0 8px 16px -8px rgba(0,0,0,.3), 0 -6px 16px -6px rgba(0,0,0,.025);
+  overflow: auto;
+
 }
 .home ~ .menu {
   transform: translateX(0%);
@@ -66,5 +77,13 @@ a {
   margin: 4px 0;
   text-decoration: none;
   display: inline-block;
+}
+.tongue {
+  position: fixed;
+  width: 25px;
+  height: 100%;
+  left: 100%;
+  top: 0;
+  background-color: rgba(0,0,0,0);
 }
 </style>
